ef27c5bacc506dfa2890ac04a61bef6a63ddce63
chay <aicha.ouattara@laplateforme.io>
2020-11-23 14:45:30 +0100

    update

--- pages/inscription.php
+++ pages/inscription.php
@@ -1,3 +1,45 @@
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname=livreor;charset=utf8', 'root', '');
+if(isset($_POST["submit"]))
+{
+    if(!empty($_POST["login"]) AND !empty($_POST["password"]) AND !empty($_POST["password2"]))
+    {
+        $login = htmlspecialchars($_POST["login"]);
+        $password = ($_POST["password"]);
+        $password2 = ($_POST["password2"]);
+        if(isset($_POST['login']))
+        {
+            $req = $bdd ->prepare('SELECT id FROM utilisateurs WHERE login = ?');
+            $req->execute(array($login));
+            $user = $req->rowCount();
+
+            if($user==0)
+            {
+                if($password == $password2)
+                {
+                    $password3 = password_hash($password, PASSWORD_BCRYPT, array('cost' => 10));
+                    $req = $bdd->prepare("INSERT INTO utilisateurs(login, password) VALUES(?, ?)");
+                    $req->execute(array($login, $password3));
+                    header('Location:connexion.php');
+                }
+                else
+                {
+                    $error = "Les mots de passe sont identiques !";
+                }
+            }
+            else
+            {
+                $error = "Votre login existe deja !";
+            }
+        }
+    }
+    else
+    {
+        $error = "Tous les champs ne sont vides !";
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -27,11 +69,19 @@
                         <input type="password" name="password" id="password" placeholder="Votre mot de passe " required>
                     </div>
                     <div>
-<!--                        <label for="password">Confirmation du mot de passe</label>-->
-                        <input type="password" name="password" id="password" placeholder="Confirmation du mot de passe " required>
+<!--                        <label for="password2">Confirmation du mot de passe</label>-->
+                        <input type="password" name="password2" id="password2" placeholder="Confirmation du mot de passe " required>
                     </div>
                     <input type="submit" name="submit" value="Inscription">
                 </div>
+            <div class="php">
+            <?php
+            if(isset($error))
+            {
+                echo $error;
+            }
+            ?>
+            </div>
         </form>
         <!--End form -->
     </article>
